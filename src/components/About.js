8b705e950944e9f7db35dd782f181376cb2eead5
Michael Benson <michaelbenson@AnnLaptop.guest.corp.microsoft.com>
2020-01-24 15:31:37 -0800

    clear up formatting

--- src/components/About.js
+++ src/components/About.js
@@ -1,39 +1,37 @@
-import React from 'react';
-import { Page, Grid, Column } from '@microsoft/fast-layouts-react';
+import React from "react";
+import { Page, Grid, Column } from "@microsoft/fast-layouts-react";
 
-import ProfileImage from './ProfileImage';
-import BioSection from './BioSection';
+import ProfileImage from "./ProfileImage";
+import BioSection from "./BioSection";
 
-import data from '../data';
+import data from "../data";
 
-const About = (props) => {
-    const sections = data.sections;
+const About = props => {
+  const sections = data.sections;
 
-    return(
-        <Page>
-            <Grid gutter={24}>
-                <Column
-                    span={data.image.span}
-                    // position={data.image.position}
-                    >
-                    <ProfileImage imageDetail={data.image} />
-                </Column>
-                {sections.map((section) => {
-                    return <Column
-                    key={section.id}
-                    span={section.span}
-                    // position={section.position}
-                    >
-                        <BioSection
-                            heading={section.heading}
-                            text={section.text} />
-                    </Column>
-                }
-
-                )}
-            </Grid>
-        </Page>
-    )
-}
+  return (
+    <Page>
+      <Grid gutter={24}>
+        <Column
+          span={data.image.span}
+          // position={data.image.position}
+        >
+          <ProfileImage imageDetail={data.image} />
+        </Column>
+        {sections.map(section => {
+          return (
+            <Column
+              key={section.id}
+              span={section.span}
+              // position={section.position}
+            >
+              <BioSection heading={section.heading} text={section.text} />
+            </Column>
+          );
+        })}
+      </Grid>
+    </Page>
+  );
+};
 
 export default About;
