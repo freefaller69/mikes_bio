7086b7d7782528d55c41c992cf36524ef11246e8
Michael Benson <michaelbenson@admins-MacBook-Pro.local>
2020-01-26 10:26:11 -0800

    convert About component to class based

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { Page, Grid, Column } from "@microsoft/fast-layouts-react";
 
 import ProfileImage from "./ProfileImage";
@@ -6,32 +6,45 @@ import BioSection from "./BioSection";
 
 import data from "../data";
 
-const About = props => {
-  const sections = data.sections;
+class About extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            sections: []
+        }
+    }
 
-  return (
-    <Page>
-      <Grid gutter={24}>
-        <Column
-          span={data.image.span}
-          // position={data.image.position}
-        >
-          <ProfileImage imageDetail={data.image} />
-        </Column>
-        {sections.map(section => {
-          return (
-            <Column
-              key={section.id}
-              span={section.span}
-              // position={section.position}
-            >
-              <BioSection heading={section.heading} text={section.text} />
-            </Column>
+    componentDidMount() {
+        this.setState({
+            sections: data.sections
+        })
+    }
+
+    render() {
+        return (
+            <Page>
+              <Grid gutter={24}>
+                <Column
+                  span={data.image.span}
+                  // position={data.image.position}
+                >
+                  <ProfileImage imageDetail={data.image} />
+                </Column>
+                {this.state.sections.map(section => {
+                  return (
+                    <Column
+                      key={section.id}
+                      span={section.span}
+                      // position={section.position}
+                    >
+                      <BioSection heading={section.heading} text={section.text} />
+                    </Column>
+                  );
+                })}
+              </Grid>
+            </Page>
           );
-        })}
-      </Grid>
-    </Page>
-  );
+    }
 };
 
 export default About;
