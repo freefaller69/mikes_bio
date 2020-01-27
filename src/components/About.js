cfa1fb9b9c253fe1c21bfd84b860e7962eed890d
Michael Benson <michaelbenson@admins-MacBook-Pro.local>
2020-01-26 20:04:02 -0800

    Try Select component

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Page, Grid, Column } from "@microsoft/fast-layouts-react";
+import { Select, SelectOption } from "@microsoft/fast-components-react-msft";
 
 import ProfileImage from "./ProfileImage";
 import BioSection from "./BioSection";
@@ -7,44 +8,82 @@ import BioSection from "./BioSection";
 import data from "../data";
 
 class About extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            sections: []
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      sections: [],
+      activeSection: 1
+    };
 
-    componentDidMount() {
-        this.setState({
-            sections: data.sections
-        })
-    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    render() {
-        return (
-            <Page>
-              <Grid gutter={24}>
-                <Column
-                  span={data.image.span}
-                  // position={data.image.position}
-                >
-                  <ProfileImage imageDetail={data.image} />
-                </Column>
+  componentDidMount() {
+    this.setState({
+      sections: data.sections,
+      activeSection: 1
+    });
+  }
+
+  handleChange(event) {
+    // this.setState({activeSection: event.target.value});
+    console.log('event', event);
+  }
+
+  handleSubmit(event) {
+    // alert('Your favorite flavor is: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <Page>
+        <Grid gutter={24}>
+          <Column
+            span={data.image.span}
+          // position={data.image.position}
+          >
+            <ProfileImage imageDetail={data.image} />
+          </Column>
+          <Column span={7}>
+            <form onSubmit={this.handleSubmit}>
+              <Select
+                placeholder={"Select a section"}
+                onChange={this.handleChange}
+              >
                 {this.state.sections.map(section => {
                   return (
-                    <Column
+                    <SelectOption
                       key={section.id}
-                      span={section.span}
-                      // position={section.position}
-                    >
-                      <BioSection heading={section.heading} text={section.text} />
-                    </Column>
+                      id={section.id}
+                      value={section.id}
+                      displayString={section.heading}
+                    />
                   );
                 })}
-              </Grid>
-            </Page>
-          );
-    }
-};
+              </Select>
+
+            </form>
+          </Column>
+          {/* <Column>
+            <BioSection heading={section.heading} text={section.text} />
+          </Column> */}
+          {this.state.sections.map(section => {
+            return (
+              <Column
+                key={section.id}
+                span={section.span}
+              // position={section.position}
+              >
+                <BioSection heading={section.heading} text={section.text} />
+              </Column>
+            );
+          })}
+        </Grid>
+      </Page>
+    );
+  }
+}
 
 export default About;
