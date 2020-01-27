81bdc2355d9550efc9d908918ad7607b1ab148d2
Michael Benson <michaelbenson@RHARI-SURFACELAPTOP.guest.corp.microsoft.com>
2020-01-27 10:34:32 -0800

    Add Select component to choose section

--- src/components/About.js
+++ src/components/About.js
@@ -10,30 +10,25 @@ import data from "../data";
 class About extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       sections: [],
-      activeSection: 1
+      selectedSection: 1,
     };
 
     this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
     this.setState({
       sections: data.sections,
-      activeSection: 1
     });
   }
 
   handleChange(event) {
-    // this.setState({activeSection: event.target.value});
-    console.log('event', event);
-  }
-
-  handleSubmit(event) {
-    // alert('Your favorite flavor is: ' + this.state.value);
-    event.preventDefault();
+    this.setState({
+      selectedSection: event
+    });
   }
 
   render() {
@@ -42,15 +37,13 @@ class About extends Component {
         <Grid gutter={24}>
           <Column
             span={data.image.span}
-          // position={data.image.position}
           >
             <ProfileImage imageDetail={data.image} />
           </Column>
           <Column span={7}>
-            <form onSubmit={this.handleSubmit}>
               <Select
-                placeholder={"Select a section"}
-                onChange={this.handleChange}
+                placeholder={"Early Years"}
+                onValueChange={this.handleChange}
               >
                 {this.state.sections.map(section => {
                   return (
@@ -63,22 +56,18 @@ class About extends Component {
                   );
                 })}
               </Select>
-
-            </form>
           </Column>
-          {/* <Column>
-            <BioSection heading={section.heading} text={section.text} />
-          </Column> */}
           {this.state.sections.map(section => {
-            return (
-              <Column
-                key={section.id}
-                span={section.span}
-              // position={section.position}
-              >
-                <BioSection heading={section.heading} text={section.text} />
-              </Column>
-            );
+            if (section.id === this.state.selectedSection) {
+              return (
+                <Column
+                  key={section.id}
+                  span={section.span}
+                >
+                  <BioSection heading={section.heading} text={section.text} />
+                </Column>
+              );
+            }
           })}
         </Grid>
       </Page>
--- src/data.js
+++ src/data.js
@@ -1,15 +1,13 @@
 const data = {
   image: {
-    span: 3,
-    position: 2,
+    span: 5,
     src: "https://placehold.it/200x200/3E3E3E/171717",
     alt: "Placeholder image"
   },
   sections: [
     {
       id: 1,
-      span: 9,
-      position: 5,
+      span: 12,
       heading: "Early Years",
       text: [
         "Originally hailing from the mean streets of Ballard, Mike’s family moved to Whidbey Island when he was 9.  While not far from Seattle, it seemed worlds away from the crowds and traffic.  He attended school there through his freshman year but finished high school at Seattle Prep on Capitol Hill.  From there he attended Seattle University and then the University of Washington, studying history and language but also spending a fair amount of time in the computer lab."
@@ -18,7 +16,6 @@ const data = {
     {
       id: 2,
       span: 12,
-      position: 2,
       heading: "Career History",
       text: [
         "Mike enjoys travel and has had the opportunity to visit a number of places, including the Caribbean, Mexico, and Maui.  He has also been to Las Vegas a few times, including a road trip where his car blew a head gasket in the middle of the Mojave Desert on the way back.",
@@ -28,7 +25,6 @@ const data = {
     {
       id: 3,
       span: 12,
-      position: 2,
       heading: "Hobbies",
       text: [
         "Mike has worked in a variety of fields.  He started off in the restaurant industry.  Seeking new challenges, he moved to the print industry working in a pre-press shop producing film for a number of publications such as For Rent magazine.  It was during this time that Mike learned HTML and CSS.  Following that, he worked in customer support for AT&T wireless when the first data capable phones came on the market and were text only.  From there he found his way into payroll and government accounting work where he spent a number of years, although his favorite thing to do in this role was finding ways to automate routine tasks in Excel and Access for the benefit of himself and the teams he was part of.  He also put his technical skills to use doing freelance tech support and building stating web pages outside of his day job and really enjoyed the challenge of designing and building things.",
